b76fa637b1d03becbe38c7365b9217aa7203ecb1
asjadanis <asjadanis128@gmail.com>
2021-08-19 16:10:32 +0500

    Updates

---
+++ precache-manifest.a4d5dea06987e9ebaba23d0a60f309d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b039e68ff4cb6eef119",
+    "url": "/3d-scene/static/css/main.bd5ca9c0.chunk.css"
+  },
+  {
+    "revision": "9b039e68ff4cb6eef119",
+    "url": "/3d-scene/static/js/main.9b039e68.chunk.js"
+  },
+  {
+    "revision": "d4730665b2d6732433a7",
+    "url": "/3d-scene/static/js/1.d4730665.chunk.js"
+  },
+  {
+    "revision": "f499ffdb72a26ac31063",
+    "url": "/3d-scene/static/js/runtime~main.f499ffdb.js"
+  },
+  {
+    "revision": "779d4f4ce41e6e2871335d4302d2e4b4",
+    "url": "/3d-scene/index.html"
+  }
+];
